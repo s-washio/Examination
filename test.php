e4cb03f86d14709c7dfc6695d0207bf996bfe9d6
s-washio <s-washio@tayutau.co.jp>
2014-04-15 15:17:14 +0900

    update test

--- test.php
+++ test.php
@@ -1,26 +1,7 @@
 <?php
 
 echo "test\n";
-echo "test\n";
-echo "test\n";
-echo "test\n";
 
-echo "test1\n";
-echo "test2\n";
-echo "test3\n";
-
-echo "test5\n";
-echo "test6\n";
-
-// Topic Modify
-echo "test6\n";
-echo "test6\n";
-
-// Topic Modify 2
-echo "test6\n";
-
-// Topic Modify 3
-echo "test6\n";
-
+// main 
 
 ?>
